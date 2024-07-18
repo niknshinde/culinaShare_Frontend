8c1e6709c4311cfc15189bc9b31cedb93d2f5e5b
Nikhil Shinde <104404955+niknshinde@users.noreply.github.com>
2024-07-18 11:23:33 +0530

    Update.js

--- src/component/chatbot/ChatBot.js
+++ src/component/chatbot/ChatBot.js
@@ -25,7 +25,7 @@ function ChatBot() {
     // Send POST request
     try {
       const response = await fetch(
-        'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=AIzaSyCYBGkT26p6d3jKJICSnyxeRPMqQMC6tHE',
+        'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=',
         {
           method: 'POST',
           headers: {
